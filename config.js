dec8864847b621e315b5435b31c84b1ec7f05c68
uTop1aDigital <107229771+uTop1aDigital@users.noreply.github.com>
2022-06-10 11:02:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'uTop1a Paradise', // 网站标题
+  nickName: "乌托邦天堂", // 昵称
+  githubName: 'uTop1aDigital', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: 'uTop1aDigital的博客',
+  SEO_keywords: 'uTop1aDigital,uTop1aDigital\'s blog,我,我的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
